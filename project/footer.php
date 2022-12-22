7410228c5086da9edb5a38187177c30c19859cf6
FAHIMSHAKIL102 <114309870+FAHIMSHAKIL102@users.noreply.github.com>
2022-12-23 02:03:42 +0600

    Add files via upload
    sgsg

---
+++ project/footer.php
@@ -0,0 +1,32 @@
+  <footer>
+    <div class="footer">
+      <div class="container">
+      
+
+
+        <div class="col-md-4 col-md-offset-4 pull-right">
+          <div class="copyright">
+           
+            <div class="credits">
+            
+              <a>Tutor-Finder</a> 
+            </div>
+			        &copy; Developed By ----<br>
+                <h3>Md. Sifatullah</h3>
+                <h3>Fahim Shakil</h3>
+                <h3>Nitta Ranjan Sarker</h3>
+                <h3>SM Sayem</h3>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+
+
+
+
+
+</body>
+
+</html>
